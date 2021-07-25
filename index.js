137709ee6180a83fc407a95ac2654c8cfe7c2f59
Aditi-Kothari <75415494+Aditi-Kothari@users.noreply.github.com>
2021-07-25 19:46:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    $("#menu").click(function(){
+      $(this).toggleClass("fa-times");
+      $("header").toggleClass('toggle');
+      $(".top").toggle();
+    });
+
+    $(window).on('scroll load',function(){
+        $("#menu").removeClass("fa-times");
+        $("header").removeClass('toggle');
+
+        if($(window).scrollTop() >0){
+          $('.top').show();
+        }else{
+          $('.top').hide();
+        }
+
+    });
+});
